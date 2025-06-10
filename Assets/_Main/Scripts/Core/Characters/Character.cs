3d98d2123e5437a4f48a8217e8ca38e9da7b7afe
Aestreuss <euphyxia@gmail.com>
2025-06-11 01:31:25 +1200

    um

--- Assets/_Main/Scripts/Core/Characters/Character.cs
+++ Assets/_Main/Scripts/Core/Characters/Character.cs
@@ -1,5 +1,7 @@
+using DIALOGUE;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
 
 namespace CHARACTERS
@@ -7,11 +9,23 @@ namespace CHARACTERS
     public abstract class Character
     {
         public string name = "";
+        public string displayName = "";
         public RectTransform root = null;
+
+        public DialogueSystem dialogueSystem => DialogueSystem.instance;
 
         public Character(string name)
         {
             this.name = name;
+            displayName = name;
+        }
+
+        public Coroutine Say(string dialogue) => Say(new List<string> { dialogue });
+
+        public Coroutine Say(List<string> dialogue)
+        {
+            dialogueSystem.ShowSpeakerName(displayName);
+            return dialogueSystem.Say(dialogue);
         }
 
         public enum CharacterType
--- Assets/_Main/Scripts/Test Scripts/TestCharacters.cs
+++ Assets/_Main/Scripts/Test Scripts/TestCharacters.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CHARACTERS;
+using DIALOGUE;
 
 namespace TESTING
 {
@@ -10,10 +11,28 @@ namespace TESTING
         // Start is called before the first frame update
         void Start()
         {
-            Character Asterios = CharacterManager.instance.CreateCharacter("Asterios");
-            Character StoryTeller = CharacterManager.instance.CreateCharacter("StoryTeller");
-            Character Myrth = CharacterManager.instance.CreateCharacter("Myrth");
+            
+            //Character StoryTeller = CharacterManager.instance.CreateCharacter("StoryTeller");
+            //Character Myrth = CharacterManager.instance.CreateCharacter("Myrth");
 
+            StartCoroutine(Test());
+
+        }
+
+        IEnumerator Test()
+        {
+            Character Asterios = CharacterManager.instance.CreateCharacter("Asterios");
+
+            List<string> lines = new List<string>()
+            {
+                "HI",
+                "this is just a line",
+                "and another line",
+                "and the final line"
+            };
+            yield return Asterios.Say(lines);
+          
+            Debug.Log("finished");
         }
 
         // Update is called once per frame
