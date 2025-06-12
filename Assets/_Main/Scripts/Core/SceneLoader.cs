03506f73ae673d072e6235d53f809f64af692601
Aestreus <euphyxia@gmail.com>
2025-06-12 14:22:52 +1200

    m

---
+++ Assets/_Main/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Button exitButton;
+    public Button startButton;
+
+    private void Start()
+    {
+        exitButton.onClick.AddListener(ExitGame);
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("maingame");
+    }
+}
